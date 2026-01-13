0f6f1f216b9112ea62a3297e1a6cd8f151321577
aklima-kazal <aklimakajal11@gmail.com>
2026-01-14 00:50:29 +0600

    add

--- social-media-backend/controllers/upload.js
+++ social-media-backend/controllers/upload.js
@@ -1,7 +1,52 @@
+const fs = require("fs");
+const cloudinary = require("cloudinary");
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 exports.uploadImages = async (req, res) => {
   try {
+    const { path } = req.body;
+    const files = Object.values(req.files).flat();
+    const images = [];
+    for (const file of files) {
+      const url = await uploadToCloudinary(file, path);
+      images.push(url);
+      removeFile(file.tempFilePath);
+    }
     res.json("Image uploaded successfully");
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
+};
+
+const uploadToCloudinary = async (file, path) => {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      {
+        folder: path,
+      },
+      (err, res) => {
+        console.log(err);
+        if (err) {
+          removeFile(file.tempFilePath);
+          return res
+            .status(500)
+            .send({ message: "Upload to cloudinary failed" });
+        }
+        resolve({
+          url: res.secure_url,
+        });
+      }
+    );
+  });
+};
+const removeFile = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
